56408ecd3da318ab489c5469bea849933a11e2c2
Lawrence <lawrencesalinas@yahoo.com>
2022-04-14 16:35:41 -0400

    sign in page is done

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Route path='/profile' element={<SignIn/>} />
       <Route path='/offers' element={<Offers/>} />
       <Route path='/signin' element={<SignIn/>} />
-      <Route path='/signup' element={<SignUp/>} />
+      <Route path='/sign-up' element={<SignUp/>} />
       <Route path='/forgot-password' element={<ForgotPasword/>} />
     </Routes>
     <Navbar/>
