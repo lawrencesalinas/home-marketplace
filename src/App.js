65ecf3bb9e811fb66f939c1234cdcab8471f9744
Lawrence <lawrencesalinas@yahoo.com>
2022-04-22 10:15:07 -0400

    explore page is created  with images

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 import Explore from './pages/Explore';
 import Offers from './pages/Offers';  
+import Category from './pages/Category';
 import Profile from './pages/Profile';    
 import PrivateRoute from './components/PrivateRoute';
 import SignIn from './pages/SignIn';
@@ -16,10 +17,12 @@ function App() {
   <Router>
     <Routes>
       <Route path='/' element={<Explore/>} />
+      <Route path='/offers' element={<Offers/>} />
+      <Route path='/category/:categoryName' element={<Category/>} />
       <Route path='/profile' element={<PrivateRoute/>} >
         <Route path='/profile' element={<Profile/>} />
      </Route>
-      <Route path='/offers' element={<Offers/>} />
+
       <Route path='/sign-in' element={<SignIn/>} />
       <Route path='/sign-up' element={<SignUp/>} />
       <Route path='/forgot-password' element={<ForgotPasword/>} />
--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,15 +1,15 @@
-import {useLocation, useNavigate} from 'react-router-dom'
-import {getAuth, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
-import {doc, setDoc, getDoc ,serverTimestamp} from 'firebase/firestore'
-import {db} from '../firebase.config'
-import {toast} from 'react-toastify'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { doc, setDoc, getDoc, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
 import googleIcon from '../assets/svg/googleIcon.svg'
 
 function OAuth() {
   const navigate = useNavigate()
   const location = useLocation()
 
-  const onGoogleClick= async () => {
+  const onGoogleClick = async () => {
     try {
       const auth = getAuth()
       const provider = new GoogleAuthProvider()
@@ -24,11 +24,11 @@ function OAuth() {
       const docSnap = await getDoc(docRef)
 
       // check if the user exist in the database if it doesnt exist, create user
-      if(!docSnap.exists()){
+      if (!docSnap.exists()) {
         // doc takes in the db, name of the collection, and userid
         // setDoc takes in the doc and the data we want to add to the database
         await setDoc(doc(db, 'users', user.uid), {
-          name:user.displayName,
+          name: user.displayName,
           email: user.email,
           timeStamp: serverTimestamp()
         })
@@ -41,14 +41,13 @@ function OAuth() {
   }
 
 
-
   return (
     <div className='socialLogin'>
-   <p>Sign {location.pathname === '/sign-up' ? 'up' : 'in' } with</p>
-   <button className="socialIconDiv" onClick={onGoogleClick}>
-     <img className='socialIconImg' src={googleIcon} alt="google" />
-   </button>
-      </div>
+      <p>Sign {location.pathname === '/sign-up' ? 'up' : 'in'} with</p>
+      <button className="socialIconDiv" onClick={onGoogleClick}>
+        <img className='socialIconImg' src={googleIcon} alt="google" />
+      </button>
+    </div>
   )
 }
 
