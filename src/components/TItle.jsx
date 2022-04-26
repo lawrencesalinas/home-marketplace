f926459ca9e919d8c13e9f939787f5102256e1eb
Lawrence <lawrencesalinas@yahoo.com>
2022-04-25 20:24:31 -0400

    contact screen with send message using email client is working

--- src/components/TItle.jsx
+++ src/components/TItle.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import homeIcon from '../assets/svg/homeIcon.svg'
+import {Link} from 'react-router-dom'
 
 
 function TItle() {
   return (
     <div>
+      <Link to ='/'>
         <h1 className='appTitle'> <img src={homeIcon} alt="home" className='homeTitle' />Honey Comb Homes</h1>
+        </Link>
     </div>
+
   )
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,8 +5,61 @@ import {db} from '../firebase.config'
 import {toast} from 'react-toastify'
 
 function Contact() {
+const [message, setMessage] = useState('')
+const [landlord, setLanlord] = useState(null)
+const [serachParams, setSearchParams] = useSearchParams()
+
+const params = useParams()
+
+useEffect(() => {
+    const getLandlord = async() => {
+        const docRef = doc(db, 'users', params.landlordId)
+        const docSnap = await getDoc(docRef)
+
+        if(docSnap.exists()) {
+          setLanlord(docSnap.data())
+        } else {
+          toast.error('Could not get landlord data')
+        }
+    }
+    getLandlord()
+}, [params.landlordId])
+
+const onChange = (e) => {
+  setMessage(e.target.value)
+}
+
   return (
-    <div>Contact</div>
+    <div className='pageContainer'>
+      <header>
+        <p className="pageHeader">
+          Contact Landlord
+        </p>
+      </header>
+
+      {landlord !== null && (
+        <main>
+          <div className="contactLandlord">
+            <p className="landLordName">Contact {landlord?.name}</p>
+          </div>
+
+          <form  className="messageForm">
+            <div className="messageDiv">
+              <label htmlFor="message" className='messageLabel'>
+                Message
+              </label>
+              <textarea name="message" id="message" className='textarea' value={message} onChange={onChange}></textarea>
+            </div>
+
+            <a href={`mailto:${landlord.email}?Subject=${serachParams.get('listingName')}&body=${message}`}>
+              <button type='button' className="primaryButton">
+                Send Message
+              </button>
+            </a>
+          </form>
+        </main>
+      )}
+    </div>
   )
 }
 
